c7282231a6f471edb01adb2b0a70b404f8d945d8
Neomaster X <neomasterx@outlook.com>
2025-10-21 08:39:11 +0300

    #11 string.ToBytes(), ToUtf8Bytes()

--- Neomaster.Extra/Extensions/StringExtensions.cs
+++ Neomaster.Extra/Extensions/StringExtensions.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Neomaster.Extra;
 
 public static class StringExtensions
@@ -5,5 +7,15 @@ public static class StringExtensions
   public static string Truncate(this string text, int length)
   {
     return text.Length > length ? text[..length] : text;
+  }
+
+  public static byte[] ToBytes(this string text)
+  {
+    return text.Select(c => (byte)c).ToArray();
+  }
+
+  public static byte[] ToUtf8Bytes(this string text)
+  {
+    return Encoding.UTF8.GetBytes(text);
   }
 }
